e6061acede8f40d0afb1eaea759d3f677ca0d967
Ali Mansoor <alimansoor4493@gmail.com>
2023-08-05 17:22:41 +0500

    Google Maps integrated

--- lib/location_service.dart
+++ lib/location_service.dart
@@ -1,10 +1,14 @@
 import 'package:http/http.dart' as http;
 import 'dart:convert' as convert;
-class LocationService
-{
+import 'package:flutter_polyline_points/flutter_polyline_points.dart';
+
+class LocationService {
+  //Your API KEY
   final String key = 'AIzaSyDHebR2JptGMJRO8sruVFVcVm3XmhRKVv4';
-  Future<String> getPlaceId(String input)async {
-    String url = 'https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=$input&inputtype=textquery&key=$key';
+
+  Future<String> getPlaceId(String input) async {
+    String url =
+        'https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=$input&inputtype=textquery&key=$key';
     var response = await http.get(Uri.parse(url));
 
     var json = convert.jsonDecode(response.body);
@@ -13,16 +17,41 @@ class LocationService
 
     return placeId;
   }
-  Future<Map<String,dynamic>>getPlace(String input) async{
+
+  Future<Map<String, dynamic>> getPlace(String input) async {
     final placeId = await getPlaceId(input);
-    String url = 'https://maps.googleapis.com/maps/api/place/details/json?place_id=$placeId&key=$key';
+    String url =
+        'https://maps.googleapis.com/maps/api/place/details/json?place_id=$placeId&key=$key';
     var response = await http.get(Uri.parse(url));
 
     var json = convert.jsonDecode(response.body);
-    var results = json['result'] as Map<String,dynamic>;
+    var results = json['result'] as Map<String, dynamic>;
     print(results);
 
     return results;
+  }
 
+  Future<Map<String, dynamic>> getDirections(
+      String origin, String destination) async {
+    String url =
+        'https://maps.googleapis.com/maps/api/directions/json?origin=$origin&destination=$destination&key=$key';
+    var response = await http.get(Uri.parse(url));
+    var json = convert.jsonDecode(response.body);
+
+    print(json);
+
+    var result = {
+      'bounds_ne': json['routes'][0]['bounds']['northeast'],
+      'bounds_sw': json['routes'][0]['bounds']['southwest'],
+      'start_location': json['routes'][0]['legs'][0]['start_location'],
+      'end_location': json['routes'][0]['legs'][0]['end_location'],
+      'polyline': json['routes'][0]['overview_polyline']['points'],
+      'polyline_decoded': PolylinePoints()
+          .decodePolyline(json['routes'][0]['overview_polyline']['points']),
+    };
+
+    print(result);
+
+    return result;
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:flutter_polyline_points/flutter_polyline_points.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:maps/location_service.dart';
 
@@ -35,12 +36,16 @@ class MapSample extends StatefulWidget {
 class MapSampleState extends State<MapSample> {
   final Completer<GoogleMapController> _controller =
       Completer<GoogleMapController>();
-  final TextEditingController _searchController = TextEditingController();
+  final TextEditingController _originController = TextEditingController();
+  final TextEditingController _destinationController = TextEditingController();
 
-  final Set<Marker> _markers = Set<Marker>();
-  final Set<Polygon> _polygon = Set<Polygon>();
+  final Set<Marker> _markers = <Marker>{};
+  final Set<Polygon> _polygon = <Polygon>{};
+  final Set<Polyline> _polyline = <Polyline>{};
   List<LatLng> polygonLatLngs = <LatLng>[];
+
   int _polygonIdCounter = 1;
+  int _polylineIdCounter = 1;
 
   static const CameraPosition _kGooglePlex = CameraPosition(
     target: LatLng(37.42796133580664, -122.085749655962),
@@ -80,6 +85,23 @@ class MapSampleState extends State<MapSample> {
     );
   }
 
+  void _setPolyline(List<PointLatLng> points) {
+    final String polylineIdVal = 'polyline_$_polylineIdCounter';
+    _polylineIdCounter++;
+    _polyline.add(
+      Polyline(
+        polylineId: PolylineId(polylineIdVal),
+        width: 2,
+        color: Colors.blue,
+        points: points
+            .map(
+              (point) => LatLng(point.latitude, point.longitude),
+            )
+            .toList(),
+      ),
+    );
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -94,39 +116,78 @@ class MapSampleState extends State<MapSample> {
           Row(
             children: [
               Expanded(
-                  child: TextFormField(
-                controller: _searchController,
-                textCapitalization: TextCapitalization.words,
-                decoration: const InputDecoration(hintText: 'Search by City'),
-                onChanged: (value) {
-                  print(value);
-                },
-              )),
+                child: Column(
+                  children: [
+                    TextFormField(
+                      controller: _originController,
+                      decoration: const InputDecoration(hintText: 'Origin'),
+                      onChanged: (value) {
+                        print(value);
+                      },
+                    ),
+                    TextFormField(
+                      controller: _destinationController,
+                      decoration:
+                          const InputDecoration(hintText: 'Destination'),
+                      onChanged: (value) {
+                        print(value);
+                      },
+                    ),
+                  ],
+                ),
+              ),
               IconButton(
                 onPressed: () async {
-                  var place =
-                      await LocationService().getPlace(_searchController.text);
-                  _goToplace(place);
+                  var directions = await LocationService().getDirections(
+                      _originController.text, _destinationController.text);
+
+                  _goToplacefordestination(
+                      directions['start_location']['lat'],
+                      directions['start_location']['lng'],
+                      directions['bounds_ne'],
+                      directions['bounds_sw']);
+
+                  _setPolyline(directions['polyline_decoded']);
+
+                  //for getting place only
+                  // var place =
+                  // await LocationService().getPlace(_searchController.text);
+                  // _goToplace(place);
                 },
                 icon: const Icon(Icons.search),
               ),
             ],
           ),
+          // Row(
+          //   children: [
+          //     Expanded(
+          //       child: TextFormField(
+          //         controller: _searchController,
+          //         decoration: const InputDecoration(hintText: 'Search by City'),
+          //         onChanged: (value) {
+          //           print(value);
+          //         },
+          //       ),
+          //     ),
+          //
+          //   ],
+          // ),
           Expanded(
             child: GoogleMap(
               markers: _markers,
               polygons: _polygon,
+              polylines: _polyline,
               mapType: MapType.normal,
               initialCameraPosition: _kGooglePlex,
               onMapCreated: (GoogleMapController controller) {
                 _controller.complete(controller);
               },
-              onTap:(point){
+              onTap: (point) {
                 setState(() {
                   polygonLatLngs.add(point);
                   _setPolygon();
                 });
-              } ,
+              },
             ),
           ),
         ],
@@ -134,14 +195,34 @@ class MapSampleState extends State<MapSample> {
     );
   }
 
-  Future<void> _goToplace(Map<String, dynamic> place) async {
-    final double latitude = place['geometry']['location']['lat'];
-    final double longitude = place['geometry']['location']['lng'];
+  // Future<void> _goToplace(Map<String, dynamic> place) async {
+  //   final double latitude = place['geometry']['location']['lat'];
+  //   final double longitude = place['geometry']['location']['lng'];
+  //
+  //   final GoogleMapController controller = await _controller.future;
+  //   await controller.animateCamera(CameraUpdate.newCameraPosition(
+  //     CameraPosition(target: LatLng(latitude, longitude), zoom: 12),
+  //   ));
+  //   //
+  //   _setMarker(LatLng(latitude, longitude));
+  // }
 
+  Future<void> _goToplacefordestination(double latitude, double longitude,
+      Map<String, dynamic> boundsNe, Map<String, dynamic> boundsSw) async {
     final GoogleMapController controller = await _controller.future;
-    await controller.animateCamera(CameraUpdate.newCameraPosition(
-      CameraPosition(target: LatLng(latitude, longitude), zoom: 12),
-    ));
+    controller.animateCamera(
+      CameraUpdate.newCameraPosition(
+        CameraPosition(target: LatLng(latitude, longitude), zoom: 12),
+      ),
+    );
+    controller.animateCamera(
+      CameraUpdate.newLatLngBounds(
+          LatLngBounds(
+            southwest: LatLng(boundsSw['lat'], boundsSw['lng']),
+            northeast: LatLng(boundsNe['lat'], boundsNe['lng']),
+          ),
+          25),
+    );
     //
     _setMarker(LatLng(latitude, longitude));
   }
@@ -150,5 +231,4 @@ class MapSampleState extends State<MapSample> {
 //   final GoogleMapController controller = await _controller.future;
 //   await controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
 // }
-
 }
